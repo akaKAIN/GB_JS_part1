c92bfd0f3bb4f0dddb70e437e301e33b681bc708
akaKAIN <mkain@mail.ru>
2020-08-06 08:30:06 +1000

    Init task_2

---
+++ lesson_6/task_2/task_2.ts
@@ -0,0 +1,38 @@
+class Slider {
+    mainImage: Element;
+    body: Element[];
+
+    constructor(products: Element[]) {
+        this.body = products
+        this.mainImage = products[0]
+    }
+
+    /**
+     * Метод присваивает переменой класса mainImage следующий элемент массива
+     * в переменную body или нулевой, если массив закончился.
+     */
+    nextImage(): void {
+        const index = this.body.indexOf(this.mainImage)
+        if (index === -1) {
+            return
+        }
+        const validIndex = index + 1 <= this.body.length - 1 ? index + 1 : 0
+        this.mainImage = this.body[validIndex]
+    }
+
+    /**
+     * Метод присваивает переменой класса mainImage предыдущий элемент массива
+     * в переменную body или последний, если массив закончился.
+     */
+    previousImage(): void {
+        const index = this.body.indexOf(this.mainImage)
+        if (index === -1) {
+            return
+        }
+        const validIndex = index - 1 <= 0 ? this.body.length - 1 : index - 1
+        this.mainImage = this.body[validIndex]
+    }
+
+
+
+}
