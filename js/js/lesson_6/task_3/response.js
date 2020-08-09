a5857e909b0dcfebff0c2bed6e267b34a3545790
akaKAIN <mkain@mail.ru>
2020-08-09 19:16:07 +1000

    Over done task_3

---
+++ js/js/lesson_6/task_3/response.js
@@ -0,0 +1,9 @@
+"use strict";
+const staticPath = './img/';
+const products = [
+    { id: 1, title: 'Milk', image: '', price: 89, count: 11 },
+    { id: 2, title: 'Bread', image: '', price: 43, count: 6 },
+    { id: 3, title: 'Coca', image: '', price: 120, count: 400 },
+    { id: 4, title: 'Egg', image: '', price: 23, count: 30 },
+    { id: 5, title: 'Salt', image: '', price: 300, count: 2 }
+];
---
+++ js/lesson_6/task_3/response.js
@@ -0,0 +1,12 @@
+"use strict";
+const response = {
+    user: 'user',
+    static: './img/',
+    products: [
+        { id: 1, title: 'Milk', image: 'milk.jpeg', price: 89, count: 11 },
+        { id: 2, title: 'Bread', image: 'bread.jpeg', price: 43, count: 6 },
+        { id: 3, title: 'Coca', image: 'Coca.jpeg', price: 120, count: 400 },
+        { id: 4, title: 'Egg', image: 'egg.jpeg', price: 23, count: 30 },
+        { id: 5, title: 'Salt', image: 'salt.jpg', price: 300, count: 2 }
+    ]
+};
